5df6d2a349ac793d2e4f9cd1aaebd80844905ce8
xpan <xpanvictor@gmail.com>
2023-09-06 17:37:58 +0100

    feat: pattern matching for refactoring

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,32 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
-    let request_uri = fetch_request_uri(&http_request[0]);
+    // Now we need just first line of the request stream
+    let http_request = buf_reader.lines().next().unwrap().unwrap();
+
+    let request_uri = fetch_request_uri(&http_request);
     println!("Got request for: {request_uri}");
 
-    if request_uri == "/" {
-        let status_line = "HTTP/1.1 200 OK";
-
-        // fetch the html file
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len(); // have to send the length of the response message body
-
-        // formatted response
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, file_name) = if request_uri == "/" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        stream.write_all(status_line.as_bytes()).unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    // fetch the html file
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len(); // have to send the length of the response message body
+
+    // formatted response
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn fetch_request_uri<'a>(request_line: &'a str) -> &'a str {
