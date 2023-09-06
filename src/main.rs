6f85531d9fbc1d529fd5322aa4bae6dab5463ff9
xpan <xpanvictor@gmail.com>
2023-09-06 17:00:16 +0100

    feat: reading and writing to an http stream

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-   let tcp_listener = TcpListener::bind("127.0.0.1:4567").unwrap();
+    let tcp_listener = TcpListener::bind("127.0.0.1:4567").unwrap();
 
-   for stream in tcp_listener.incoming() {
-       let _stream = stream.unwrap();
+    for stream in tcp_listener.incoming() {
+        let stream = stream.unwrap();
 
-       println!("Connection established!")
-   }
+        handleConnection(stream);
+    }
+}
+
+fn handleConnection(mut stream: TcpStream) {
+    let bufReader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = bufReader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 400 Bad\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
